b19d18efe2e941702fecd5d7c27031370394d2b3
Amit Pandey <37929815+bottest123121@users.noreply.github.com>
2019-11-19 10:20:37 +0530

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,14 +1,14 @@
 module.exports = Object.freeze({
-  TOKEN: '1036025488:AAGSZqUYZlVpfTWhIsOqBBDI7dLI12qrNYQ',
+  TOKEN: '988708348:AAGzi26Y_S-bJdlN_-RAn581P7b6FXskbWs',
   ARIA_SECRET: 'Amitp@ndey123121',
-  ARIA_DOWNLOAD_LOCATION: '/home/amit/dn',
+  ARIA_DOWNLOAD_LOCATION: '/app/dn',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
   ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'], // Prevent downloading from URLs containing these substrings
   ARIA_FILTERED_FILENAMES: ['YIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
   GDRIVE_PARENT_DIR_ID: '1_jPdK5jijeeImovHYH5Zl2KAm0Uatb7W',
   SUDO_USERS: [401643149, 464848593],	// Telegram user IDs. These users can use the bot in any chat.
-  AUTHORIZED_CHATS: [678, 901],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  AUTHORIZED_CHATS: [-301889115],	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
     ENABLED: false,
